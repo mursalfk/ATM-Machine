1b2e577734e119bb9e9ffa8ba61af3c9dcda1cfd
Mursal Furqan <36442744+mursalfk@users.noreply.github.com>
2021-01-23 19:41:49 +0500

    Updated more to Django

--- atm_py/urls.py
+++ atm_py/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.welcome, name="welcome-atm"),
-    path('login/', views.login, name="login-atm"),
-    path('signup/', views.signup, name="signup-atm"),
+    path('login/', views.loginpage, name="login-atm"),
+    path('signup/', views.signuppage, name="signup-atm"),
     path('dashboard/', views.dashboard, name="dashboard-atm"),
-
-    
+    path('admin/', admin.site.urls, name='top-atm'),
+    path('logout/', views.logoutUser, name = 'logout-user'),
 
 ]
--- atm_py/views.py
+++ atm_py/views.py
@@ -1,14 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def welcome(request):
     return render(request, 'atm_py/welcome.html', {'title':'Welcome'})
 
-def login(request):
-    return render(request, 'atm_py/login.html', {'title':'Login'})
+def loginpage(request):
+    context = {'title':'Login'}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
 
-def signup(request):
-    return render(request, 'atm_py/signup.html', {'title':'Signup'})
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard-atm')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+            return render(request, 'atm_py/login2.html', context)
+        
+
+    
+    return render(request, 'atm_py/login2.html', context)
+def logoutUser(request):
+    logout(request)
+    return redirect('login-atm')
+
+
+def signuppage(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account Created for ' + user)
+
+            return redirect('/')
+
+    context = {'form' : form, 'title':'Signup'}
+    # return render(request, 'atm_py/signup.html', {'title':'Signup'}, context)
+    return render(request, 'atm_py/signup2.html', context)
+
 
 def dashboard(request):
     return render(request, 'atm_py/dashboard.html', {'title':'Dashboard'})
