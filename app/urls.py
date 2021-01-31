befc538725794c7dbab96db5f027e56484c65289
Mursal Furqan <36442744+mursalfk@users.noreply.github.com>
2021-01-31 14:18:25 +0500

    removed free space

--- app/urls.py
+++ app/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('splash/', views.welcome, name="welcome-atm"),
     path('dashboard/', views.dashboard, name="dashboard-atm"),
     path('balance_inquiry/', views.balance_inquiry, name="balance_inquiry_atm"),
-    
     path('withdraw_money/', views.withdraw_money, name="withdraw_money_atm"),
     path('tranc_history/', views.tranc_history, name="tranc_history_atm"),
     path('change_password/', views.change_password, name="change_password_atm"),
