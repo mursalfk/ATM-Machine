eaa26ddbddfce0737bc4cf158ae94f2ba41a30bb
Mursal Furqan <36442744+mursalfk@users.noreply.github.com>
2021-01-31 09:15:59 +0500

    removed Change Password Button

--- atm/settings.py
+++ atm/settings.py
@@ -145,5 +145,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER =  'stoicsfornation@gmail.com'
-EMAIL_HOST_PASSWORD = 'Mursal-50' 
+EMAIL_HOST_USER =  '''Your Email goes here'''
+EMAIL_HOST_PASSWORD = '''Password Goes Here'''
